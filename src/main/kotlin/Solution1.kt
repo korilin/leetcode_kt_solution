36c58f1c8ae73b677ffbc2747e942d0f8a43a7b7
korilin <korilin.dev@gmail.com>
2021-03-17 03:06:10 +0800

    Solution1

---
+++ src/main/kotlin/Solution1.kt
@@ -0,0 +1,16 @@
+class Solution1 {
+    /**
+     * 使用哈希表来进行匹配，时间复杂度 O(n)
+     */
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val hashMap = HashMap<Int, Int>()
+        for ((index, num) in nums.withIndex()) {
+            val get = hashMap[target - num]
+            if (get != null) {
+                return intArrayOf(get, index)
+            }
+            hashMap[num] = index
+        }
+        return intArrayOf()
+    }
+}
