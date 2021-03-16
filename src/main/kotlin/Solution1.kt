9b8b8ad61954da4df1c6cfff4c29085c6318fc9c
korilin <korilin.dev@gmail.com>
2021-03-17 04:38:33 +0800

    Solution 2
    - 添加第二题
    - 添加注释说明

--- src/main/kotlin/Solution1.kt
+++ src/main/kotlin/Solution1.kt
@@ -1,6 +1,17 @@
+/**
+ * Title: 两数之和
+ *
+ * Description:
+ * 给定一个整数数组 num 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那两个整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ *
+ * 你可以按任意顺序返回答案。
+ *
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ */
 class Solution1 {
     /**
-     * 使用哈希表来进行匹配，时间复杂度 O(n)
+     * 使用哈希表的key来进行匹配，时间复杂度 O(n)
      */
     fun twoSum(nums: IntArray, target: Int): IntArray {
         val hashMap = HashMap<Int, Int>()
---
+++ src/main/kotlin/Solution2.kt
@@ -0,0 +1,47 @@
+/**
+ * Title: 两数相加
+ *
+ * Description:
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ */
+class Solution2 {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        var node1 = l1
+        var node2 = l2
+        var sum: Int
+        var carry = 0
+
+        val l3 = ListNode(0)
+        var now: ListNode? = null
+
+        while (node1 != null || node2 != null || carry == 1) {
+            sum = (node1?.`val` ?: 0) + (node2?.`val` ?: 0) + carry
+            carry = sum / 10
+            now = if (now == null) {
+                l3.also {  l3.`val` = sum  % 10 }
+            } else {
+                ListNode(sum % 10).also { now!!.next = it }
+            }
+            node1 = node1?.next
+            node2 = node2?.next
+        }
+        return l3
+    }
+}
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
