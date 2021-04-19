1877720c78cfed36158d67085e72d58f11a3ec09
korilin <korilin.dev@gmail.com>
2021-04-19 21:45:35 +0800

    Create Solution27.kt
    Solution 27 code

---
+++ src/main/kotlin/q1_50/q21_30/Solution27.kt
@@ -0,0 +1,11 @@
+package q1_50.q21_30
+
+class Solution27 {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        var index = 0
+        for (num in nums) {
+            if (num != `val`) nums[index++] = num
+        }
+        return index
+    }
+}
