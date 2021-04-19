be3af04225544d08e04468a8ae271c2dad130b62
korilin <korilin.dev@gmail.com>
2021-04-19 21:22:34 +0800

    Solution26
    solution 26 code

---
+++ src/main/kotlin/q1_50/q21_30/Solution25.kt
@@ -0,0 +1,12 @@
+package q1_50.q21_30
+
+
+class Solution25 {
+    fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
+        return null
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+}
---
+++ src/main/kotlin/q1_50/q21_30/Solution26.kt
@@ -0,0 +1,15 @@
+package q1_50.q21_30
+
+class Solution26 {
+    fun removeDuplicates(nums: IntArray): Int {
+        if (nums.size <= 1) return nums.size
+        var index = 1
+        var preNum: Int = nums[0]
+        for (move in 1 until nums.size) {
+            val num = nums[move]
+            if (num != preNum) nums[index++] = num
+            preNum = num
+        }
+        return index
+    }
+}
