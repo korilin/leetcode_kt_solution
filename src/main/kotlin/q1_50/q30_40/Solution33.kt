a971576bbfcc75817311c0b993e8fbeee40f60f6
korilin <korilin.dev@gmail.com>
2021-07-03 23:49:09 +0800

    Create Solution33.kt

---
+++ src/main/kotlin/q1_50/q30_40/Solution33.kt
@@ -0,0 +1,20 @@
+package q1_50.q30_40
+
+class Solution33 {
+    fun search(nums: IntArray, target: Int): Int {
+        var offset = 1
+        var index = 0
+
+        while (0 <= index && index < nums.size) {
+            nums[index].also {
+                when {
+                    target == it -> return index
+                    offset == -1 && target > it -> return -1
+                    offset == 1 && target < it -> index = nums.size.also { offset = -1 }
+                }
+            }
+            index += offset
+        }
+        return -1
+    }
+}
