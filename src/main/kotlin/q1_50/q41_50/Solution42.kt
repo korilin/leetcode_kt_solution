d287e020c7b94252789a2cca1fb8b0e8335eb93b
korilin <korilin.dev@gmail.com>
2021-07-09 11:02:17 +0800

    Solution283

---
+++ src/main/kotlin/q1_50/q41_50/Solution42.kt
@@ -0,0 +1,4 @@
+package q1_50.q41_50
+
+class Solution42 {
+}
---
+++ src/main/kotlin/q251_q300/q281_290/Solution283.kt
@@ -0,0 +1,23 @@
+package q251_q300.q281_290
+
+class Solution283 {
+    fun moveZeroes(nums: IntArray): Unit {
+        var zeroIndex = -1
+        var currentIndex = 0
+
+        while (currentIndex < nums.size) {
+
+            if (zeroIndex != -1 && nums[currentIndex] != 0) {
+                nums[zeroIndex] = nums[currentIndex]
+                nums[currentIndex] = 0
+                zeroIndex++
+            }
+
+            if (zeroIndex == -1 && nums[currentIndex] == 0) {
+                zeroIndex = currentIndex
+            }
+
+            currentIndex++
+        }
+    }
+}
