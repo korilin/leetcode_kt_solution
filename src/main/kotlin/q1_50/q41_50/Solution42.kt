57fac9818d17e3dfe06121342a2db1378e6c94e0
korilin <korilin.dev@gmail.com>
2021-07-09 11:45:11 +0800

    Update Solution42.kt

--- src/main/kotlin/q1_50/q41_50/Solution42.kt
+++ src/main/kotlin/q1_50/q41_50/Solution42.kt
@@ -1,4 +1,24 @@
 package q1_50.q41_50
 
+/**
+ * https://leetcode-cn.com/problems/trapping-rain-water/
+ */
 class Solution42 {
+    fun trap(height: IntArray): Int {
+        var ans = 0
+        var left = 0
+        var right = height.size - 1
+        var leftMax = 0
+        var rightMax = 0
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                leftMax = maxOf(height[left], leftMax)
+                ans += leftMax - height[left++]
+            }else{
+                rightMax = maxOf(height[right], rightMax)
+                ans += rightMax - height[right--]
+            }
+        }
+        return ans
+    }
 }
