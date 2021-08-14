2812065daaf6de880ec9b261d232b8032be9979e
korilin <korilin.dev@gmail.com>
2021-08-14 19:58:18 +0800

    Create 10-2.kt

---
+++ src/main/kotlin/offer2/10-2.kt
@@ -0,0 +1,11 @@
+package offer2
+
+fun numWays(n: Int): Int {
+    if (n < 2) return 1
+    var v1 = 1
+    var v2 = 2
+    for (i in 3..n) {
+        v2 = ((v1 + v2) % 1000000007).also { v1 = v2 }
+    }
+    return v2
+}
