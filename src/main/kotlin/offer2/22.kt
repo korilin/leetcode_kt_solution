6018ecc1ffcca97bbd56b0c91ed9f3c6d6fc6799
korilin <korilin.dev@gmail.com>
2021-08-31 22:16:29 +0800

    Update 22.kt

--- src/main/kotlin/offer2/22.kt
+++ src/main/kotlin/offer2/22.kt
@@ -1,2 +1,12 @@
 package offer2
 
+fun getKthFromEnd(head: ListNode?, k: Int): ListNode? {
+    var current = head ?: return null
+    var latter = head
+    repeat(k) { latter = latter!!.next ?: return current }
+    while (latter != null) {
+        current = current.next!!
+        latter = latter!!.next
+    }
+    return current
+}
