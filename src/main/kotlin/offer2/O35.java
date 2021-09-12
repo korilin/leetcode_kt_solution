534d8f08b5c9dae1ae73a8027602e621fe817a11
korilin <korilin.dev@gmail.com>
2021-09-12 19:22:28 +0800

    Create O35.java

---
+++ src/main/kotlin/offer2/O35.java
@@ -0,0 +1,42 @@
+package offer2;
+
+
+public class O35 {
+    public Node copyRandomList(Node head) {
+
+        if (head == null) return null;
+
+        for (Node node = head; node != null; node = node.next.next) {
+            Node newNode = new Node(node.val);
+            newNode.next = node.next;
+            node.next = newNode;
+        }
+
+        for (Node node = head; node != null; node = node.next.next) {
+            Node random = node.random;
+            node.next.random = random == null ? null : random.next;
+        }
+
+        Node newHead = head.next;
+        for (Node node = head; node != null; node = node.next) {
+            // 回置原链表指针
+            Node newNode = node.next;
+            node.next = newNode.next;
+            newNode.next = newNode.next == null ? null : newNode.next.next;
+        }
+
+        return newHead;
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
